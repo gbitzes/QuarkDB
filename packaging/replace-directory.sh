3b035022860f49f0e6d0c1555e9fd414c5de2e4e
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-07-07 14:36:38 +0200

    Add automatic generation of documentation through CI

---
+++ packaging/replace-directory.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+STAGING_AREA="$1"
+TARGET="$2"
+OLD_CONTENTS="${TARGET}-old"
+
+test ! -d "$TARGET" || mv "$TARGET" "$OLD_CONTENTS"
+mv "$STAGING_AREA" "$TARGET"
+test ! -d "$OLD_CONTENTS" || rm -rf "$OLD_CONTENTS"
+
