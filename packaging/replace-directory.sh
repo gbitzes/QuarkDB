e7a06434d93399cd52f4eadae0cfebc70d10c7b5
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-07-10 09:32:34 +0200

    Workaround for failing docs job

--- packaging/replace-directory.sh
+++ packaging/replace-directory.sh
@@ -6,6 +6,8 @@ TARGET="$2"
 OLD_CONTENTS="${TARGET}-old"
 
 test ! -d "$TARGET" || mv "$TARGET" "$OLD_CONTENTS"
+
+sleep 30
 mv "$STAGING_AREA" "$TARGET"
 test ! -d "$OLD_CONTENTS" || rm -rf "$OLD_CONTENTS"
 
