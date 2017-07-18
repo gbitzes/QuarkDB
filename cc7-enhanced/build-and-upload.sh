60392ae0b2292d84d41a80210f9918d5e0297cf7
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-07-18 13:24:30 +0200

    Try to use gitlabci-docker-builder

--- cc7-enhanced/build-and-upload.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker login -u gitlab-ci-token -p $CI_BUILD_TOKEN gitlab-registry.cern.ch
-docker build -t gitlab-registry.cern.ch/eos/quarkdb/cc7-enhanced .
-docker push gitlab-registry.cern.ch/eos/quarkdb/cc7-enhanced
-
