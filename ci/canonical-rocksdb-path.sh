712d3a5c2f1bede9c4139ea36ade3b7ba4701a5e
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-07-19 16:06:26 +0200

    CI: Fix rocksdb cached build path

--- ci/canonical-rocksdb-path.sh
+++ ci/canonical-rocksdb-path.sh
@@ -2,8 +2,7 @@
 set -e
 
 # Discover rocksdb commit hash
-cd deps/rocksdb
-ROCKSDB_COMMIT=$(git rev-parse HEAD)
+ROCKSDB_COMMIT=$(git --git-dir deps/rocksdb/.git rev-parse HEAD)
 
 # Emit the system-wide path for this rocksdb commit
 printf "/rocksdb/$ROCKSDB_COMMIT"
--- ci/ubuntu/asan.sh
+++ ci/ubuntu/asan.sh
@@ -3,11 +3,14 @@
 # This step ought to be a no-op usually, thanks to our custom ubuntu image.
 ci/ubuntu/prepare.sh
 
+# Determine rocksdb cached build
+ROCKSDB_CACHED_BUILD=$(./ci/rocksdb-cmake.sh)
+
 # Build QuarkDB with AddressSanitizer
 git submodule update --init --recursive
 rm -rf build
 mkdir build && cd build
-CXXFLAGS='-fsanitize=address' cmake -DTESTCOVERAGE=ON -DXROOTD_ROOT_DIR=/xrootd/install -DLIBRARY_PATH_PREFIX=lib ..
+CXXFLAGS='-fsanitize=address' cmake -DTESTCOVERAGE=ON -DXROOTD_ROOT_DIR=/xrootd/install -DLIBRARY_PATH_PREFIX=lib ${ROCKSDB_CACHED_BUILD} ..
 make
 ./test/quarkdb-tests
 ./test/quarkdb-stress-tests
