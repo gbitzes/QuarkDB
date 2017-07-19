aaeef1b8f89039ba2bd7e2a03a3ba0277090918d
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-07-19 10:19:43 +0200

    CI: Add scripts for fedora image preparation

---
+++ ci/fedora/prepare.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+##------------------------------------------------------------------------------
+## Bootstrap packages - needed to run 'builddep' on quarkdb for the next
+## step.
+##------------------------------------------------------------------------------
+
+dnf install -y gcc-c++ cmake3 make rpm-build which git yum-plugin-priorities yum-utils
+
+##------------------------------------------------------------------------------
+## Extract quarkdb build dependencies from its specfile.
+##------------------------------------------------------------------------------
+
+mkdir build
+pushd build
+cmake3 .. -DPACKAGEONLY=1
+make srpm
+yum-builddep -y SRPMS/*
+popd
