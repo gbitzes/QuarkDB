571e57c43c56462cda217f13838cdd4e689da8c4
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-10-04 20:41:08 +0200

    Fix one more subtle bug in the tests, ensure node is caught up before taking checkpoint
    When taking a checkpoint on node #0, it might be the only
    one which hasn't caught up with the leader.. need to check first.

--- test/e2e.cc
+++ test/e2e.cc
@@ -150,6 +150,9 @@ TEST_F(Raft_e2e, simultaneous_clients) {
   std::string err;
   std::string checkpointPath = SSTR(commonState.testdir << "/checkpoint");
 
+  // Before taking a checkpoint, ensure node #0 is caught up
+  RETRY_ASSERT_TRUE(journal(0)->getCommitIndex() == journal(leaderID)->getCommitIndex());
+
   ASSERT_TRUE(dispatcher()->checkpoint(checkpointPath, err));
   ASSERT_FALSE(dispatcher()->checkpoint(checkpointPath, err)); // exists already
 
