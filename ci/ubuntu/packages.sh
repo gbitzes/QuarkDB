58bcc9ab4acb87cb3f9ab2287b321b33fc6011ce
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-10-10 13:47:28 +0200

    Add jemalloc as runtime dependency

--- ci/ubuntu/packages.sh
+++ ci/ubuntu/packages.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-apt-get install -y git g++ cmake zlib1g-dev openssl libssl-dev libhiredis-dev python libbz2-dev lcov uuid-dev
+apt-get install -y git g++ cmake zlib1g-dev openssl libssl-dev libhiredis-dev python libbz2-dev lcov uuid-dev libjemalloc-dev
