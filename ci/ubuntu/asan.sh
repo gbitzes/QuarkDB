e2d4aabcf825cb09cfd6b2dc27982ee5b6c3423d
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-08-10 10:23:31 +0200

    Increase raft timeouts on ASan job

--- ci/ubuntu/asan.sh
+++ ci/ubuntu/asan.sh
@@ -10,6 +10,6 @@ rm -rf build
 mkdir build && cd build
 CXXFLAGS='-fsanitize=address' cmake -DTESTCOVERAGE=ON -DXROOTD_ROOT_DIR=/xrootd/install -DLIBRARY_PATH_PREFIX=lib ..
 make
-./test/quarkdb-tests
+QDB_TEST_TIMEOUT=default ./test/quarkdb-tests
 ./test/quarkdb-stress-tests
 make coverage-report
