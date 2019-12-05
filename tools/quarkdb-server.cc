8516356fc1282ceec04520816c90373809c8c141
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-12-05 12:33:54 +0100

    Add --configuration-dir to quarkdb-server for conveniently launching localhost test clusters

--- tools/quarkdb-server.cc
+++ tools/quarkdb-server.cc
@@ -27,8 +27,14 @@
 #include "Configuration.hh"
 #include "QuarkDBNode.hh"
 #include "netio/AsioPoller.hh"
+#include "ShardDirectory.hh"
+#include "StateMachine.hh"
 #include "EventFD.hh"
 #include "../deps/CLI11.hpp"
+#include "utils/Uuid.hh"
+#include "utils/DirectoryIterator.hh"
+
+#include <dirent.h>
 
 namespace quarkdb {
 
@@ -38,6 +44,76 @@ EventFD shutdownFD;
 static void handle_sigint(int sig) {
   inFlightTracker.setAcceptingRequests(false);
   shutdownFD.notify();
+}
+
+void createTestCluster(const std::string &optConfigurationDir, int port) {
+  quarkdb::mkpath_or_die(optConfigurationDir, 0755);
+
+  std::string clusterID = generateUuid();
+
+  std::vector<quarkdb::RaftServer> nodes;
+  for(size_t i = 0; i < 3; i++) {
+    nodes.emplace_back("localhost", port+i);
+  }
+
+  for(size_t i = 0; i < 3; i++) {
+
+    std::string dataDir = SSTR(optConfigurationDir << "data-" << port);
+    std::string configDir = SSTR(optConfigurationDir << "config-" << port);
+
+    std::ostringstream ss;
+    ss << "xrd.port " << port << std::endl;
+    ss << "xrd.protocol redis:" << port << "libXrdQuarkDB.so" << std::endl;
+    ss << "redis.mode raft" << std::endl;
+    ss << "redis.database " << dataDir << std::endl;
+    ss << "redis.myself localhost:" << port << std::endl;
+
+    write_file_or_die(configDir, ss.str());
+    std::unique_ptr<quarkdb::ShardDirectory> shardDirectory;
+
+    quarkdb::Status st;
+    shardDirectory.reset(quarkdb::ShardDirectory::create(dataDir, clusterID, "default", nodes, 0, {}, st));
+
+    port++;
+  }
+}
+
+int runCluster(const std::string &execPath, const std::string &optConfigurationDir, int port) {
+  std::string err;
+  if(!quarkdb::directoryExists(optConfigurationDir, err)) {
+    createTestCluster(optConfigurationDir, port);
+  }
+
+  //----------------------------------------------------------------------------
+  // Locate configuraiton files
+  //----------------------------------------------------------------------------
+  std::vector<std::string> configurationFiles;
+  quarkdb::DirectoryIterator iter(optConfigurationDir);
+
+  struct dirent *entry;
+  while( (entry = iter.next()) ) {
+    if(entry->d_type != DT_DIR && StringUtils::startsWith(entry->d_name, "config-")) {
+      configurationFiles.emplace_back(SSTR(optConfigurationDir << entry->d_name));
+    }
+  }
+
+  //----------------------------------------------------------------------------
+  // Create new tmux session..
+  //----------------------------------------------------------------------------
+  std::string sessionId = generateUuid();
+  system(SSTR("tmux -2 new-session -d -s \"" << sessionId << "\"").c_str());
+
+  for(size_t i = 0; i < configurationFiles.size(); i++) {
+    if(i != 0) {
+      system("tmux split-window -v");
+    }
+
+    system(SSTR("tmux send-keys \"" << execPath << " --configuration " << configurationFiles[i] << " \" C-m").c_str());
+  }
+
+  system("tmux select-layout even-vertical");
+  execlp("tmux", "tmux", "-2", "attach-session", "-d", (char*) NULL);
+  return errno;
 }
 
 int runServer(const std::string &optConfiguration) {
@@ -79,8 +155,8 @@ int runServer(const std::string &optConfiguration) {
 
 }
 
-struct ExistenceValidator : public CLI::Validator {
-  ExistenceValidator() : Validator("PATH") {
+struct FileExistenceValidator : public CLI::Validator {
+  FileExistenceValidator() : Validator("PATH") {
     func_ = [](const std::string &path) {
 
       std::string err;
@@ -98,29 +174,53 @@ int main(int argc, char** argv) {
   // Setup variables
   //----------------------------------------------------------------------------
   CLI::App app("QuarkDB is a distributed datastore with a redis-like API. quarkdb-server is the main server executable.");
-  ExistenceValidator existenceValidator;
+  FileExistenceValidator fileExistenceValidator;
 
   std::string optConfiguration;
+  std::string optConfigurationDir;
+  int clusterPort = 4444;
 
   //----------------------------------------------------------------------------
   // Setup options
   //----------------------------------------------------------------------------
   app.add_option("--configuration", optConfiguration, "Path to configuration file")
-    ->required()
-    ->check(existenceValidator);
+    ->check(fileExistenceValidator);
+
+  app.add_option("--configuration-dir", optConfigurationDir, "Path to configuration directory to launch local test cluster - requires to have tmux installed.");
+  app.add_option("--test-cluster-port", clusterPort, "The port to use when creating a local test cluster -- ignored if cluster configuration already existed.");
 
   //----------------------------------------------------------------------------
   // Parse..
   //----------------------------------------------------------------------------
   try {
     app.parse(argc, argv);
+
+    if(optConfiguration.empty() && optConfigurationDir.empty()) {
+      std::cerr << "Either --configuration or --configuration-dir must be specified." << std::endl;
+      return 1;
+    }
   } catch (const CLI::ParseError &e) {
     return app.exit(e);
   }
 
   //----------------------------------------------------------------------------
+  // Run cluster
+  //----------------------------------------------------------------------------
+  if(!optConfigurationDir.empty()) {
+    if(optConfigurationDir[optConfigurationDir.size()-1] != '/') {
+      optConfigurationDir += "/";
+    }
+
+    return quarkdb::runCluster(argv[0], optConfigurationDir, clusterPort);
+  }
+
+  //----------------------------------------------------------------------------
   // Run server.
   //----------------------------------------------------------------------------
-  return quarkdb::runServer(optConfiguration);
+  if(!optConfiguration.empty()) {
+    return quarkdb::runServer(optConfiguration);
+  }
+
+  qdb_throw("should never reach here");
 }
 
