24520549974cabc6c5285e592803da99c44f005a
Georgios Bitzes <georgios.bitzes@cern.ch>
2020-05-18 12:39:47 +0200

    ci: install tzdata non-interactively in ubuntu image

--- ci/ubuntu/packages.sh
+++ ci/ubuntu/packages.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
+export TZ=Europe/Zurich
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get install -y git g++ cmake zlib1g-dev openssl libssl-dev python python3 libbz2-dev lcov uuid-dev libjemalloc-dev libdw-dev libdw1 liblz4-dev libzstd-dev
